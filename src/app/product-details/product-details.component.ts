fd1ad9233937cddba343bde7a293205aa7d1dc95
bmoes-coderoom <bmboe001@odu.edu>
2020-02-03 15:35:18 -0500

    Added ShoppingCart and ShippingPrices

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service'
 
 @Component({
   selector: 'app-product-details',
@@ -12,6 +13,7 @@ export class ProductDetailsComponent implements OnInit {
 product;
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
@@ -19,5 +21,10 @@ product;
     this.product = products[+params.get('productId')];
   });
 }
+
+ addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
 
 }
